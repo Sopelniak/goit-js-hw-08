c8ef30f17bca6b494cbfeaa6d2fd51ae9ad2eb9b
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-05 02:03:42 +0300

    01 gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.reduce(
+  (acc, item) =>
+    acc +
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+</a>`,
+  ''
+);
+
+gallery.innerHTML = markup;
+
+function createLightbox() {
+  const gallery = new SimpleLightbox('.gallery a');
+
+  gallery.on('show.simplelightbox', function () {
+    // captionDelay = 250;
+  });
+}
+createLightbox();
