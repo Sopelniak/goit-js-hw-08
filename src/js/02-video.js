af6458f590ae589216ca54d5d868b7518abaf192
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-06 04:20:08 +0300

    update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,42 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
 });
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  }, 1000)
+);
+
+try {
+  const dataValue = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  const seconds = dataValue.seconds;
+  // console.log(seconds);
+  player
+    .setCurrentTime(seconds)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+} catch (e) {
+  console.log(e);
+}
